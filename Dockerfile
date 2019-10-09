e8f0c9b84b9d8728ec17646f6e3683d665d42a89
joshweir <joshua.weir@gmail.com>
2019-10-10 07:51:41 +1100

    add gunicorn

--- Dockerfile
+++ Dockerfile
@@ -10,18 +10,6 @@ RUN apt-get update && apt-get install -y \
     git &&\
     apt-get -q clean -y && rm -rf /var/lib/apt/lists/* && rm -f /var/cache/apt/*.bin
 
-FROM python:3.6
-
-RUN apt-get update && apt-get install -y \
-    build-essential \
-    libssl-dev \
-    supervisor \
-    curl \
-    nginx \
-    python-dev \
-    git &&\
-    apt-get -q clean -y && rm -rf /var/lib/apt/lists/* && rm -f /var/cache/apt/*.bin
-
 RUN pip install spacy==2.1.0
 
 RUN python -m spacy download en_core_web_sm
@@ -68,8 +56,12 @@ RUN pip install -r requirements.txt
 #   mv /neuralcoref-cache/neuralcoref.tar.gz /neuralcoref-cache/f46bc05a4bfba2ae0d11ffd41c4777683fa78ed357dc04a23c67137abf675e14.7d6f9a6fecf5cf09e74b65f85c7d6896b21decadb2554d486474f63b95ec4633 &&\
 #   cd
 
-ADD ./src/ /app
+ADD ./ /app
 
 WORKDIR /app
 
-CMD python /app/server.py
+CMD gunicorn --bind 0.0.0.0:80 \
+  --worker-tmp-dir /dev/shm \
+  --workers=2 --threads=4 --worker-class=gthread \
+  --log-file=- \
+  wsgi:app
--- server.py
+++ server.py
@@ -17,22 +17,22 @@ pool = Pool(1, maxtasksperchild=50)
 
 parse = ChProcess('en_core_web_sm')
 
-@app.route('/', methods = ['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
   data = request.data.decode('utf-8')
   if not data:
-      return Response(status=500, response="no data")
+    return Response(status=500, response="no data")
 
   print("got something: '%s'" % data)
   print(datetime.datetime.utcnow())
-  result = pool.apply(parse.to_json, [data, request.args])
-  print('fin')
-  print(datetime.datetime.utcnow())
+  # result = pool.apply(parse.to_json, [data, request.args])
+  result = parse.to_json(data, request.args)
+  print(datetime.datetime.utcnow(), 'fin')
 
   return Response(
-    status=200,
-    response=json.dumps(result),
-    content_type="application/json")
+      status=200, response=json.dumps(result), content_type="application/json")
+
 
 if __name__ == '__main__':
-    app.run(port=port, host="0.0.0.0")
+  app.run(port=port, host="0.0.0.0")
